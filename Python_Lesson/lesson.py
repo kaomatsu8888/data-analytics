1427a0da54f2f349a3a47ea1d92dd2754ba7d5d6
kaomatsu8888 <m.kaoru0830@gmail.com>
2023-12-12 08:23:27 +0900

    PythonLesson

---
+++ Python_Lesson/lesson.py
@@ -0,0 +1,16 @@
+print("hello world")
+
+# 変数の宣言
+num = 1
+print(num)
+
+name = "Mike"
+print(name)
+
+# 変数の型
+num = 1
+name = "Mike"
+print(num, type(num)) # type関数で型を表示.intは整数
+print(name, type(name)) # type関数で型を表示.strは文字列
+
+
